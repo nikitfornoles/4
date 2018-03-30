5b2b9172e49fe9c44e4c933ac8891b6dbdd360b5
nikitfornoles <cfornoles@gbox.adnu.edu.ph>
2018-03-30 23:34:06 +0800

    Initial commit

---
+++ 4/EmployeeDtr.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class EmployeeDtr
+    {
+        private List <String> record_id;
+        private List <String> date;
+        private List <String> employeeID;
+        private List <String> am_in;
+        private List <String> am_out;
+        private List <String> pm_in;
+        private List <String> pm_out;
+        private List <String> ot_in;
+        private List <String> ot_out;
+        private List <String> duration;
+
+        public EmployeeDtr ()
+        {
+            record_id = new List<String>();
+            date = new List<String>();
+            employeeID = new List<String>();
+            am_in = new List<String>();
+            am_out = new List<String>();
+            pm_in = new List<String>();
+            pm_out = new List<String>();
+            ot_in = new List<String>();
+            ot_out = new List<String>();
+            duration = new List<String>();
+        }
+
+        public void setID (String id) {
+            record_id.Add(id);
+        }
+        public void setDate (String key) {
+            date.Add(key);
+        }
+        public void setEmployeeID (String id) {
+            employeeID.Add(id);
+        }
+        public void parseKey(String key) {
+            String [] key_component = key.Split(':');
+            setEmployeeID(key_component[0]);
+            setDate(key_component[1]);
+        }
+        public void setAMin (String s) {
+            am_in.Add(s);
+        }
+        public void setAMout (String s) {
+            am_out.Add(s);
+        }
+        public void setPMin(String s) {
+            pm_in.Add(s);
+        }
+        public void setPMout(String s) {
+            pm_out.Add(s);
+        }
+        public void setOTin(String s) {
+            ot_in.Add(s);
+        }
+        public void setOTout(String s) {
+            ot_out.Add(s);
+        }
+        public void setDuration(String s) {
+            duration.Add(s);
+        }
+        public void addRecord (String key) {
+            setID(key); //record_id
+            parseKey(key); //date and employeeID
+
+            //set to "null" and not null for testing purposes
+            setAMin("null");
+            setAMout("null");
+            setPMin("null");
+            setPMout("null");
+            setOTin("null");
+            setOTout("null");
+            setDuration("null");
+        }
+        public int getRecordIDCount () {
+            return record_id.Count;
+        }
+        public int getRecordIndex (String s) {
+            int index = -1;
+            for (int i = 0; i < getRecordIDCount(); i++) {
+                if (record_id[i] == s) {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
+        }
+        public bool hasRecordID(String s) {
+            bool ans = false;
+            int index = getRecordIndex(s);
+            ans = (getRecordIndex(s) == -1 ? false : true);
+            return ans;
+        }
+        public void modifyAMin(String key, String time) {
+            int record_index = getRecordIndex(key);
+            am_in[record_index] = time;
+        }
+        public void modifyAMout(String key, String time) {
+            int record_index = getRecordIndex(key);
+            am_out[record_index] = time;
+        }
+        public bool issetAMout (String key) {
+            bool isset = false;
+            int record_index = getRecordIndex(key);
+            if (am_out[record_index] != "null") {
+                isset = true;
+            }
+            return isset;
+        }
+        public void modifyPMin(String key, String time) {
+            int record_index = getRecordIndex(key);
+            pm_in[record_index] = time;
+        }
+        public void modifyPMout(String key, String time) {
+            int record_index = getRecordIndex(key);
+            pm_out[record_index] = time;
+        }
+        public void modifyOTin(String key, String time)
+        {
+            int record_index = getRecordIndex(key);
+            ot_in[record_index] = time;
+        }
+        public void modifyOTout(String key, String time)
+        {
+            int record_index = getRecordIndex(key);
+            ot_out[record_index] = time;
+        }
+        public void modifyDuration(int i, String d)
+        {
+            duration [i] = d;
+        }
+        public String computeDuration (int i, int sec_duration, int min_duration, int hour_duration, String timecode)
+        {
+            String[] time_in = am_in[i].Split(':');
+            String[] time_out = am_out[i].Split(':');
+
+            int hour_in = 0;
+            int hour_out = 0;
+            int min_in = 0;
+            int min_out = 0;
+            int sec_in = 0;
+            int sec_out = 0;
+
+            if (timecode == "AM") {
+                time_in = am_in[i].Split(':');
+                time_out = am_out[i].Split(':');
+            }
+            else if (timecode == "PM") {
+                time_in = pm_in[i].Split(':');
+                time_out = pm_out[i].Split(':');
+            }
+            else if (timecode == "OT") {
+                time_in = ot_in[i].Split(':');
+                time_out = ot_out[i].Split(':');
+            }
+            hour_in = Convert.ToInt32(time_in[0]);
+            min_in = Convert.ToInt32(time_in[1]);
+            sec_in = Convert.ToInt32(time_in[2]);
+
+            hour_out = Convert.ToInt32(time_out[0]);
+            min_out = Convert.ToInt32(time_out[1]);
+            sec_out = Convert.ToInt32(time_out[2]);
+
+            //compute duration [seconds]
+            if (sec_out < sec_in) {
+                if (min_out == 0) {
+                    hour_out = hour_out - 1;
+                    min_out = min_out + 60;
+                }
+                min_out = min_out - 1;
+                sec_out = sec_out + 60;
+            }
+            sec_duration = sec_duration + (sec_out - sec_in);
+
+            //compute duration [minutes]
+            if (min_out < min_in) {
+                hour_out = hour_out - 1;
+                min_out = min_out + 60;
+            }
+            min_duration = min_duration + (min_out - min_in);
+
+            //compute duration [hours]
+            hour_duration = hour_duration + (hour_out - hour_in);
+            return (hour_duration + ":" + min_duration + ":" + sec_duration);
+        }
+        public void computeTotalDuration () {
+            for (int i = 0; i < getRecordIDCount(); i++) {
+                int sec_duration = 0;
+                int min_duration = 0;
+                int hour_duration = 0;
+                String ans = "";
+                String[] duration_component;
+
+                //compute AM duration
+                if (am_in[i] != "null" && am_out[i] != "null") {
+                    ans = computeDuration (i, sec_duration, min_duration, hour_duration, "AM");
+                    duration_component = ans.Split(':');
+                    hour_duration = Convert.ToInt32(duration_component[0]);
+                    min_duration = Convert.ToInt32(duration_component[1]);
+                    sec_duration = Convert.ToInt32(duration_component[2]);
+                }
+
+                //compute PM duration
+                if (pm_in[i] != "null" && pm_out[i] != "null") {
+                    ans = computeDuration(i, sec_duration, min_duration, hour_duration, "PM");
+                    duration_component = ans.Split(':');
+                    hour_duration = Convert.ToInt32(duration_component[0]);
+                    min_duration = Convert.ToInt32(duration_component[1]);
+                    sec_duration = Convert.ToInt32(duration_component[2]);
+                }
+
+                //compute OT duration
+                if (ot_in[i] != "null" && ot_out[i] != "null") {
+                    ans = computeDuration(i, sec_duration, min_duration, hour_duration, "OT");
+                    duration_component = ans.Split(':');
+                    hour_duration = Convert.ToInt32(duration_component[0]);
+                    min_duration = Convert.ToInt32(duration_component[1]);
+                    sec_duration = Convert.ToInt32(duration_component[2]);
+                }
+                int addend = 0;
+                if (sec_duration >= 60) {
+                    addend = sec_duration / 60;
+                    min_duration = min_duration + addend;
+                    sec_duration = sec_duration % 60;
+                }
+                if (min_duration >= 60) {
+                    addend = min_duration / 60;
+                    hour_duration = hour_duration + addend;
+                    min_duration = min_duration % 60;
+                }
+
+                String duration = Convert.ToString(hour_duration) + ":" + Convert.ToString(min_duration) + ":" + Convert.ToString(sec_duration);
+                modifyDuration(i, duration);
+            }
+        }
+        public void writeToCSV(String output_repository) {
+            computeTotalDuration();
+            for (int i = 0; i < getRecordIDCount (); i++) {
+                using (StreamWriter outputFile = new StreamWriter(output_repository + @"\DTR.csv", true)) {
+                    outputFile.WriteLine(date[i] + "," + employeeID[i] +  "," + am_in[i] + "," + am_out[i] + "," + pm_in[i] + "," + pm_out[i] + "," + ot_in[i] + "," + ot_out[i] + "," + duration[i]);
+                }                
+            }
+        }
+    }
+}
---
+++ 4/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        	//necessary variables from text file
+            String line_EnNo = "";
+            String line_Mode = "";
+            String line_DateTime = "";
+
+            List<String> header = new List<String>();
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"D:\00nikit\ACUMATICA\Exercise\4\GLG_001.TXT");
+            string output_repository = (@"D:\00nikit\ACUMATICA\Exercise\4\");
+
+            int counter = 0;
+            String line = "";
+
+            EmployeeDtr dtr = new EmployeeDtr();
+            while ((line = file.ReadLine()) != null)
+            {
+                String[] str_arr = line.Split('\t');
+                if (counter == 0) {
+            		for (int i = 0; i < str_arr.Length; i++) {
+                        header.Add(str_arr[i]);
+                    }
+                    //print new header in a csv file
+                    using (StreamWriter outputFile = new StreamWriter(output_repository + @"\DTR.csv")) {
+                        outputFile.WriteLine("Date, Employee ID, AM IN, AM OUT, PM IN, PM OUT, OT IN, OT OUT, Duration");
+                    }
+            	}
+            	else {
+                    //get values
+                    for (int i = 0; i < str_arr.Length; i++) {
+                        if (header[i] == "EnNo")
+                            line_EnNo = str_arr[i];
+                        if (header[i] == "Mode")
+                            line_Mode = str_arr[i];
+                        if (header[i] == "DateTime")
+                            line_DateTime = str_arr[i];
+                    }                    	
+                }
+                if (line_Mode == "5" || line_Mode == "6") {
+                    String[] dateTime = line_DateTime.Split(' ');
+                    String date = dateTime[0];
+                    String time = dateTime[1];
+                    String key = line_EnNo + ":" + date;
+                    if (!dtr.hasRecordID(key)) {
+                    	/* Records record_id, date, and employeeID
+                    	Set all other attributes to null for further processing of next lines
+                    	*/
+                    	dtr.addRecord(key);
+                    }
+                    TimeSetter(line_Mode, key, date, time, dtr);
+                }                
+            	System.Console.WriteLine(line);
+                counter++;
+            }
+
+            file.Close();
+            System.Console.WriteLine("There were {0} lines.", counter);
+
+            //Write to CSV File
+            dtr.writeToCSV(output_repository);
+            // Suspend the screen.  
+            System.Console.ReadLine();
+        }
+        private static void TimeSetter(String mode, String key, String date, String time, EmployeeDtr dtr) {
+        	String [] time_component = time.Split(':');
+        	int hh = Convert.ToInt32(time_component[0]);
+
+        	if (mode == "5") { //IN
+        		if (hh < 12) { //AM IN
+        			dtr.modifyAMin(key, time);
+        		}
+        		else if (hh >= 12 && hh < 18) { //PM IN
+        			dtr.modifyPMin(key, time);
+        		}
+        	}
+        	else if (mode == "6") { //OUT
+        		if (hh <= 13) { //AM OUT
+                    if (hh <= 12) {
+                        dtr.modifyAMout(key, time);
+                    }
+                    else if (hh > 12) {
+                        //check first if AM OUT is null, if not, assign time to it
+                        if(!dtr.issetAMout(key)) {
+                            dtr.modifyAMout(key, time);
+                        }
+                    }
+        		}
+        		else if (hh >= 13 && hh <= 23) { //PM OUT, OT IN, OT OUT
+                    if (hh >= 18) { //OVERTIME
+                        dtr.modifyPMout(key, "18:00:00");
+                        dtr.modifyOTin(key, "18:00:00");
+                        dtr.modifyOTout(key, time);
+                    }
+                    else { //UNDERTIME
+                        dtr.modifyPMout(key, time);
+                    }
+        		}
+        	}
+        }
+    }
+}
